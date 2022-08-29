9e20d7105cf1aabdcbd39835e44195fe5ad41073
Ramiro Magno <ramiro.magno@gmail.com>
2022-08-29 21:42:11 +0100

    Add demo basic.R

---
+++ demo/basic.R
@@ -0,0 +1,31 @@
+library(glfw)
+
+# Initialize the library
+if (!glfw_init()) stop("Could not initialize GLFW")
+
+# Create a windowed mode window and its OpenGL context
+window = glfw_create_window(640L, 480L, "Hello World")
+
+# Make the window's context current
+glfw_make_context_current(window)
+
+# Loop until the user closes the window
+while (!glfw_window_should_close(window))
+{
+  # Render here
+  gl_clear(0x00004000L)
+
+  # Swap front and back buffers
+  glfw_swap_buffers(window)
+
+  # Poll for and process events
+  glfw_poll_events()
+}
+
+# Close the window
+glfw_destroy_window(window)
+
+# Terminate GLFW
+glfw_terminate()
+
+
