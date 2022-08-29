9b926dc2aa7b3f325f45dffacbf2e892fd21ac0e
Ramiro Magno <ramiro.magno@gmail.com>
2022-08-29 22:32:48 +0100

    Update demo basic.R

--- demo/basic.R
+++ demo/basic.R
@@ -1,6 +1,6 @@
 library(glfw)
 
-# Initialize the library
+# Initialize GLFW
 if (!glfw_init()) stop("Could not initialize GLFW")
 
 # Create a windowed mode window and its OpenGL context
@@ -9,11 +9,14 @@ window = glfw_create_window(640L, 480L, "Hello World")
 # Make the window's context current
 glfw_make_context_current(window)
 
+# Load OpenGL functions with GLAD
+glad_load_gl()
+
 # Loop until the user closes the window
 while (!glfw_window_should_close(window))
 {
   # Render here
-  gl_clear(0x00004000L)
+  gl_clear(gl$color_buffer_bit)
 
   # Swap front and back buffers
   glfw_swap_buffers(window)
