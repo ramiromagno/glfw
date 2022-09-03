d83d3d395605dc7e6529693da7fa5e7d4624a2dd
Ramiro Magno <ramiro.magno@gmail.com>
2022-09-03 01:36:49 +0100

    Start creating a glm API with an R implementation

---
+++ R/glm.R
@@ -0,0 +1,31 @@
+#' @export
+glm_vec3 <- function(x, y, z) {
+  c(x, y, z)
+}
+
+#' @export
+glm_vec4 <- function(x, y, z, w) {
+  c(x, y, z, w)
+}
+
+#' @export
+glm_length <- function(x) {
+  sqrt(sum(x ^ 2))
+}
+
+#' @export
+glm_normalize <- function(x) {
+  x / sqrt(sum(x ^ 2))
+}
+
+#' @export
+glm_cross <- function(x, y) {
+  c(x[2] * y[3] - x[3] * y[2],
+    x[3] * y[1] - x[1] * y[3],
+    x[1] * y[2] - x[2] * y[1])
+}
+
+#' @export
+glm_dot <- function(x, y) {
+  sum(x * y)
+}
