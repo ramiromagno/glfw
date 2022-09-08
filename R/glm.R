0422692be8bb6a9d76b639e3622dd308a2936579
Ramiro Magno <ramiro.magno@gmail.com>
2022-09-08 02:14:09 +0100

    Add `glm_rotate()`

--- R/glm.R
+++ R/glm.R
@@ -45,3 +45,36 @@ glm_scale <- function(m, v) {
 
   m
 }
+
+#' @export
+glm_rotate <- function(m, angle, axis) {
+
+  a <- angle
+  c <- cos(a)
+  s <- sin(a)
+
+  axis <- glm_normalize(axis)
+  temp <- (1 - c) * axis
+
+  rotate <- matrix(0, nrow = 4, ncol = 4)
+
+  rotate[1, 1] <- c + temp[1] * axis[1]
+  rotate[2, 1] <- temp[1] * axis[2] + s * axis[3]
+  rotate[3, 1] <- temp[1] * axis[3] - s * axis[2]
+
+  rotate[1, 2] <- temp[2] * axis[1] -s * axis[3]
+  rotate[2, 2] <- c + temp[2] * axis[2]
+  rotate[3, 2] <- temp[2] * axis[3] + s * axis[1]
+
+  rotate[1, 3] <- temp[3] * axis[1] + s * axis[2]
+  rotate[2, 3] <- temp[3] * axis[2] - s * axis[1]
+  rotate[3, 3] <- c + temp[3] * axis[3]
+
+  m_ <- matrix(0, nrow = 4, ncol = 4)
+  m_[, 1] <- m[, 1] * rotate[1, 1] + m[, 2] * rotate[2, 1] + m[, 3] * rotate[3, 1]
+  m_[, 2] <- m[, 1] * rotate[1, 2] + m[, 2] * rotate[2, 2] + m[, 3] * rotate[3, 2]
+  m_[, 3] <- m[, 1] * rotate[1, 3] + m[, 2] * rotate[2, 3] + m[, 3] * rotate[3, 3]
+  m_[, 4] <- m[, 4]
+
+  m_
+}
