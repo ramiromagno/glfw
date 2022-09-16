9124ff49bcb019144e3f91633492389b051079cf
Ramiro Magno <ramiro.magno@gmail.com>
2022-09-16 14:08:38 +0100

    Update uniform fns interface

--- R/gl_uniform.R
+++ R/gl_uniform.R
@@ -4,21 +4,26 @@ gl_get_uniform_location <- function(program, name) {
 }
 
 #' @export
-gl_uniform_f <- function(location, value) {
-  .Call(gl_uniform_f_, location, value)
+gl_uniform_fv <- function(location, value) {
+  .Call(gl_uniform_fv_, location, value)
 }
 
 #' @export
-gl_uniform_i <- function(location, value) {
-  .Call(gl_uniform_i_, location, value)
+gl_uniform_f <- function(location, v) {
+  .Call(gl_uniform_f_, location, v)
 }
 
 #' @export
-gl_uniform_ui <- function(location, value) {
-  .Call(gl_uniform_ui_, location, value)
+gl_uniform_iv <- function(location, value) {
+  .Call(gl_uniform_iv_, location, value)
 }
 
 #' @export
-gl_uniform_matrix4_f <- function(location, transpose, value) {
-  .Call(gl_uniform_matrix4_f_, location, transpose, value)
+gl_uniform_uiv <- function(location, value) {
+  .Call(gl_uniform_uiv_, location, value)
+}
+
+#' @export
+gl_uniform_matrix4_fv <- function(location, transpose, value) {
+  .Call(gl_uniform_matrix4_fv_, location, transpose, value)
 }
--- src/gl_uniform.c
+++ src/gl_uniform.c
@@ -11,7 +11,7 @@ SEXP gl_get_uniform_location_(SEXP program, SEXP name) {
   return Rf_ScalarInteger(location);
 }
 
-SEXP gl_uniform_f_(SEXP location, SEXP value) {
+SEXP gl_uniform_fv_(SEXP location, SEXP value) {
 
   GLint _location = INTEGER(location)[0];
   R_len_t _n = XLENGTH(value);
@@ -39,7 +39,47 @@ SEXP gl_uniform_f_(SEXP location, SEXP value) {
   return R_NilValue;
 }
 
-SEXP gl_uniform_i_(SEXP location, SEXP value) {
+SEXP gl_uniform_f_(SEXP location, SEXP v) {
+
+  GLint _location = INTEGER(location)[0];
+  R_len_t _n = XLENGTH(v);
+
+  switch(_n) {
+  case 1:
+    glUniform1f(_location,
+                (GLfloat) REAL(v)[0]
+                );
+    break;
+  case 2:
+    glUniform2f(_location,
+                (GLfloat) REAL(v)[0],
+                (GLfloat) REAL(v)[1]
+                );
+    break;
+  case 3:
+    glUniform3f(_location,
+                (GLfloat) REAL(v)[0],
+                (GLfloat) REAL(v)[1],
+                (GLfloat) REAL(v)[2]
+                );
+    break;
+  case 4:
+    glUniform4f(_location,
+                (GLfloat) REAL(v)[0],
+                (GLfloat) REAL(v)[1],
+                (GLfloat) REAL(v)[2],
+                (GLfloat) REAL(v)[3]
+                );
+    break;
+  default:
+    Rf_error("Length of `v` must be 1, 2, 3 or 4.\n");
+  break;
+  }
+
+  return R_NilValue;
+}
+
+SEXP gl_uniform_iv_(SEXP location, SEXP value) {
 
   R_len_t _n = LENGTH(value);
   GLint *_ptr = INTEGER(value);
@@ -66,7 +106,7 @@ SEXP gl_uniform_i_(SEXP location, SEXP value) {
   return R_NilValue;
 }
 
-SEXP gl_uniform_ui_(SEXP location, SEXP value) {
+SEXP gl_uniform_uiv_(SEXP location, SEXP value) {
 
   GLint _location = INTEGER(location)[0];
   R_len_t _n = XLENGTH(value);
@@ -94,7 +134,7 @@ SEXP gl_uniform_ui_(SEXP location, SEXP value) {
   return R_NilValue;
 }
 
-SEXP gl_uniform_matrix4_f_(SEXP location, SEXP transpose, SEXP value) {
+SEXP gl_uniform_matrix4_fv_(SEXP location, SEXP transpose, SEXP value) {
 
   GLint _location = INTEGER(location)[0];
   R_len_t _n = XLENGTH(value);
